8a5e82a1726753d022c479a085cadddf263c9908
Edgar <edgar83@gmail.com>
2016-12-04 15:07:49 -0800

    First commit

---
+++ DataStrutureProject/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Practice
+{
+    class Singleton
+    {
+        private static Singleton _instance;
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        protected Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            // Support multithreaded applications through
+            // 'Double checked locking' pattern which (once
+            // the instance exists) avoids locking each
+            // time the method is invoked
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        Console.WriteLine("Creating instance");
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
